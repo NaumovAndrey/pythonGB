a36c1232db0729eb1c35b2b0b6e526d7a2f0d62e
naumov <naumov.a.n@icloud.com>
2022-10-12 22:04:30 +0700

    дополнение к дз №2

--- homeworks/homework_2/task_4.py
+++ homeworks/homework_2/task_4.py
@@ -1,13 +1,19 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 import random
 
-number = random.randint(1, 10)
+number = random.randint(5, 10)
 _list = [i for i in range(-number, number + 1)]
-print(*_list)
+#print(*_list)
 
 arr = list()
 with open('text.txt', 'r') as file:
+    sum = 1
     for i in file:
-        arr.append(int(i))
+        n = int(i)
+        arr.append(n)
+        if len(_list) >= n:     # условие, если есть значение в массиве. Если нет - пропускаем
+            sum *= _list[n]
+        else:
+            continue
 
-print(arr)
+print(f'Умножение по индексам {arr} в массиве {_list} = {sum}')
