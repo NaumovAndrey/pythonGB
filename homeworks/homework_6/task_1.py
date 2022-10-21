0cc3cd0499e72afaba9712edbfa57b31703b99c0
naumov <naumov.a.n@icloud.com>
2022-10-21 23:52:43 +0700

    дз6

---
+++ homeworks/homework_6/task_1.py
@@ -0,0 +1,4 @@
+# список из цифр от 1 до 5 list comprehension
+
+nums = [n for n in range(1, 6)]
+print(nums)
---
+++ homeworks/homework_6/task_3.py
@@ -0,0 +1,3 @@
+# Двумерный маасив
+
+print([[x for x in range(1, 6)] for y in range(1, 6)])
