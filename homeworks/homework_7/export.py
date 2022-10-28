7063f7f5d71ab0bbd55416bb222f729fe99e36b6
naumov <naumov.a.n@icloud.com>
2022-10-28 13:43:50 +0700

    exp

--- homeworks/homework_7/export.py
+++ homeworks/homework_7/export.py
@@ -1,3 +1,6 @@
+import json
+
+
 def v_exp(li):
     v = int(input('1 - Экспортировать в ".txt"\n2 - Экспортировать в json\n -: '))
     if v == 1:
@@ -19,5 +22,6 @@ def exp_json(li):
     n = input('Введите название файла: ')
     n = n + '.json'
     with open(n, 'w', encoding='UTF-8') as file:
-        for key, value in li.items():
-            file.write('%s:%s\n' % (key, value))
+        # for key, value in li.items():
+        #     file.write('%s:%s\n' % (key, value))
+        json.dump(li, file)                             # пишет иероглифы, что не правильно?
